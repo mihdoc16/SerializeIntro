4c482904685a6f3c4edb6da3bd83e93c49fa4622
mihdoc16 <nikboyhd@gmail.com>
2018-09-28 11:58:21 +0200

    added schueler

---
+++ SerializeIntro/src/Schueler.java
@@ -0,0 +1,40 @@
+
+import java.time.LocalDate;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dominik
+ */
+public class Schueler {
+    private String name;
+    private LocalDate birthday;
+
+    public Schueler(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+    
+    
+}
